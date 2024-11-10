7fd32921c62141d26939fa23fba2a79160d6a708
brandoniscoding <brandoniscoding.dev@gmail.com>
2024-11-10 11:56:58 +0100

    feat: (#8) Initialize stack module and define Stack interface for various implementations

---
+++ stack/src/main/java/com/brandoniscoding/algorithms/StackImpl.java
@@ -0,0 +1,28 @@
+package com.brandoniscoding.algorithms;
+
+public interface StackImpl<T> {
+
+    // Method to push an element onto the stack
+    void push(T value);
+
+    // Method to pop an element onto the stack
+    T pop();
+
+    // Method to pop at the top element without remove it 
+    T peek();
+
+    // Method ti check if the is empty
+    boolean isEmpty();
+
+    // Method to get a current size of the stack
+    int size();
+
+    // Method to clear all elements from the stack
+    void clear();
+
+    // Method to search for an element in the stack and return its position from the top (1-based index)
+    int search(T element);
+
+    // Method to check if a specific element is present in the stack
+    boolean contains(T element);
+}
